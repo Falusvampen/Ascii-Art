f4f6eca6657b5fcb92857df60b1f68924dfdd10b
Kevazy <kevinstenius@gmail.ax>
2022-09-24 08:31:11 +0300

    Added support for newlines and edge cases, though more work is needed. Also refactor

--- asciiprint.go
+++ asciiprint.go
@@ -2,24 +2,33 @@ package asciiart
 
 import (
 	"fmt"
-	"strings"
 )
 
 func AsciiPrint(s string, font string) {
-	// BUILD CHARACTERS
 	fontArray := GetFont(font)
-	charArray := initalizeLines(s)
-	for _, c := range s {
-		for i, line := range GetCharacter(c, fontArray) {
-			charArray[i] += line
+	charArray := make([]string, 8)
+
+	// Loop through each character in the string
+	for i := 0; i < len(s); i++ {
+		// Get the valid character from the font
+		if s[i] != '\n' && s[i] >= 32 && s[i] <= 126 {
+			for linePos, line := range GetCharacter(rune(s[i]), fontArray) {
+				charArray[linePos+len(charArray)-8] += line
+			}
+		} else if s[i] == '\n' {
+			// If it's the first newline in a sequence, add 8 lines, otherwise 1
+			charArray = append(charArray, make([]string, 8)...)
+			if i < len(s)-1 {
+				for j := i; s[j+1] == '\n'; j++ {
+					charArray = append(charArray, make([]string, 1)...)
+					i++
+				}
+			}
+		} else {
+			fmt.Println("ERROR")
 		}
 	}
 	for _, line := range charArray {
 		fmt.Println(line)
 	}
-}
-
-func initalizeLines(s string) []string {
-	charArray := make([]string, len(s)+strings.Count(s, "\n"))
-	return charArray
 }
