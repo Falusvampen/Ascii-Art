ceecdbc5d6df18e33a170af2fbabd98254acc080
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-09-26 18:16:43 +0300

    replaceall instead of replace

--- test/main.go
+++ test/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	if len(os.Args[1:]) == 1 {
-		asciiart.AsciiPrint(strings.Replace(os.Args[1], `\n`, "\n", -1), "standard")
+		asciiart.AsciiPrint(strings.ReplaceAll(os.Args[1], `\n`, "\n"), "standard")
 	} else {
 		log.Fatal("Usage: go run main.go \"string\"")
 	}
