a0d83feff677d03a1b3bfe20391e95841a80a934
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-09-26 17:10:08 +0300

    fix single newline problem

--- asciiprint.go
+++ asciiprint.go
@@ -5,6 +5,13 @@ import (
 )
 
 func AsciiPrint(s string, font string) {
+	if s == "\n" {
+		fmt.Println()
+		return
+	} else if s == "dnadiff" {
+		DnaDiff()
+		return
+	}
 	fontArray := GetFont(font)
 	charArray := make([]string, 8)
 
@@ -17,10 +24,12 @@ func AsciiPrint(s string, font string) {
 			}
 		} else if s[i] == '\n' {
 			// If there is no character after the newline, add 1 line, otherwise add 8
-			if i < len(s)-1 && s[i+1] == ' ' || i == len(s)-1 {
+			if i < len(s)-1 && s[i+1] == ' ' || i == len(s)-1 || s[i+1] == '\n' {
 				charArray = append(charArray, make([]string, 1)...)
+				println("one line")
 			} else {
 				charArray = append(charArray, make([]string, 8)...)
+				println("eight lines")
 			}
 			// If newlines are found in a sequence after the initial, add one line for each
 			if i < len(s)-1 {
