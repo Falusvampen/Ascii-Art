30fe9f7281f46f69a17c7bcb2f1108327fcdffa1
Kevazy <kevazy@outlook.com>
2022-09-28 16:51:51 +0300

    WIP error checking

--- src/asciiart_test.go
+++ src/asciiart_test.go
@@ -16,8 +16,26 @@ var testStrings = []string{
 	"Hello\n\nThere",
 }
 
+var testAnswers = [9][]string{
+	{""},
+	{
+		" _    _          _   _         ",
+		"| |  | |        | | | |         ",
+		"| |__| |   ___  | | | |   ___   ",
+		"|  __  |  / _ \\ | | | |  / _ \\  ",
+		"| |  | | |  __/ | | | | | (_) | ",
+		"|_|  |_|  \\___| |_| |_|  \\___/  ",
+	},
+}
+
 func TestAsciiPrint(*testing.T) {
 	for _, test := range testStrings {
 		AsciiPrint(test, "standard")
+	}
+
+	for _, v := range testAnswers {
+		for _, line := range v {
+			println(line)
+		}
 	}
 }
