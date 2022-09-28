7e3f1cfa2d63d938f10f3e0d33beee99d3c7734e
Kevazy <kevazy@outlook.com>
2022-09-28 14:00:35 +0300

    Now has optional font support

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ import (
 func main() {
 	if len(os.Args[1:]) == 1 {
 		asciiart.AsciiPrint(strings.ReplaceAll(os.Args[1], `\n`, "\n"), "standard")
+	} else if len(os.Args[1:]) == 2 {
+		asciiart.AsciiPrint(strings.ReplaceAll(os.Args[1], `\n`, "\n"), os.Args[2])
 	} else {
-		log.Fatal("Usage: go run main.go \"string\"")
+		log.Fatal("Usage: go run main.go \"[input]\" [font]")
 	}
 }
