cff722ab17722fb4bb8d40783ae632828b59733c
Kevazy <kevazy@outlook.com>
2022-09-29 11:41:11 +0300

    More documentation

--- src/asciiprint.go
+++ src/asciiprint.go
@@ -41,6 +41,7 @@ func AsciiPrint(s string, font string) {
 }
 
 func initializeLines(s string) []string {
+	// Initialize the charArray with the correct amount of lines, 0 if a character is invalid, otherwise 8
 	charArray := make([]string, 0)
 	for _, c := range s {
 		if c >= 32 && c <= 126 {
