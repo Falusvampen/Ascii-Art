b94ca117a0bc14833e35f3d79a2a888b6ca148eb
Kevazy <kevazy@outlook.com>
2022-10-14 04:48:51 +0300

    New usage message

--- ascii-art-color/main.go
+++ ascii-art-color/main.go
@@ -11,6 +11,6 @@ func main() {
 	if len(os.Args[1:]) == 2 && strings.HasPrefix(os.Args[2], "--color=") {
 		asciiart.AsciiPrint(strings.ReplaceAll(os.Args[1], `\n`, "\n"), os.Args[2][8:])
 	} else {
-		fmt.Println("Usage: go run . [STRING] [OPTION]\n\nEX: go run . something --color=<color>")
+		fmt.Println("Usage: go run . \"" + os.Args[1] + "\" --color=<input>\n\nColor formats:\n--color=green:2-5 (positions)\n--color=blue:abc (letters)\n--color=yellow:1-2,red:abc (multiple)")
 	}
 }
